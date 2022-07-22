edc013d6cfff4fcc1e93fb3af5e2a06ec83bc7f2
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2022-07-22 14:26:16 +0300

    nodejs module: first simple examples

--- coursework/nodejs/index.js
+++ coursework/nodejs/index.js
@@ -1,1 +1,8 @@
-console.log("Hello from Node.js...");
+//console.log("Hello from Node.js...");
+
+// const person = require("./person");
+// console.log(person);
+
+// const Person = require("./person");
+// const person1 = new Person("John Doe", 30);
+// person1.greeting();
