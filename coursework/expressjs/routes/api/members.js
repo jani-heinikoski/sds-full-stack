5fe17779ba701de5aec957bfe0f99397ef959ced
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2022-07-27 22:54:08 +0300

    expressjs module: route for creating a new member

--- coursework/expressjs/routes/api/members.js
+++ coursework/expressjs/routes/api/members.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const uuid = require("uuid");
 const members = require("../../Members");
 
 // Get all members
@@ -16,6 +17,23 @@ router.get("/:id", (req, res) => {
         res.status(400).json({ msg: `no member with id ${req.params.id}` });
     } else {
         res.json(member);
+    }
+});
+
+// Create a member
+router.post("/", (req, res) => {
+    const newMember = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: "active",
+    };
+
+    if (!newMember.name || !newMember.email) {
+        res.status(400).json({ msg: "Please include a name and an email" });
+    } else {
+        members.push(newMember);
+        res.json(members);
     }
 });
 
