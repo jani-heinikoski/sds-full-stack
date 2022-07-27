89700ec47098e368c81a06120fbaefeaf5907ae5
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2022-07-27 22:18:37 +0300

    expressjs module: added a custom logger middleware

--- coursework/expressjs/index.js
+++ coursework/expressjs/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const path = require("path");
 const members = require("./Members");
+const logger = require("./middleware/logger");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Use the logger middleware
+// app.use(logger);
 
 // Get all members
 app.get("/api/members", (req, res) => {
