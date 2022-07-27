26a5e1ddcca141d4095c5ed1e36e305ccc4fa248
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2022-07-27 22:10:56 +0300

    expressjs module: moved hardcoded members to its own file

--- coursework/expressjs/index.js
+++ coursework/expressjs/index.js
@@ -1,33 +1,16 @@
 const express = require("express");
 const path = require("path");
+const members = require("./Members");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const members = [
-    {
-        id: 1,
-        name: "John Doe",
-        email: "john@gmail.com",
-        status: "active",
-    },
-    {
-        id: 2,
-        name: "Johnnie Boy",
-        email: "johnnieboy@gmail.com",
-        status: "active",
-    },
-    {
-        id: 3,
-        name: "Top Kek",
-        email: "w@l.com",
-        status: "inactive",
-    },
-];
 
+// Get all members
 app.get("/api/members", (req, res) => {
     res.json(members);
 });
 
+// Serve static assets from the /public path
 app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
