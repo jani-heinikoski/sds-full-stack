d6a9783a3718b18b17d67837ffa8ed01ecb66fe4
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2022-07-27 21:16:53 +0300

    nodejs module: dumb way to serve static assets

--- coursework/expressjs/index.js
+++ coursework/expressjs/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello World!</h1>");
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
