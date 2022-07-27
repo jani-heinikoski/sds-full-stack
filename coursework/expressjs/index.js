0ec488780790d431cd6711e81ea3513dde4018ac
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2022-07-27 21:18:00 +0300

    nodejs module: better way to serve static assets

--- coursework/expressjs/index.js
+++ coursework/expressjs/index.js
@@ -4,9 +4,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
     console.log("server started on port", PORT);
