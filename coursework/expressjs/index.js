707cde27aec061b97d1817df3cebba5c4beb65e6
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2022-07-27 21:27:25 +0300

    expressjs module: a dummy REST API

--- coursework/expressjs/index.js
+++ coursework/expressjs/index.js
@@ -3,6 +3,30 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const members = [
+    {
+        id: 1,
+        name: "John Doe",
+        email: "john@gmail.com",
+        status: "active",
+    },
+    {
+        id: 2,
+        name: "Johnnie Boy",
+        email: "johnnieboy@gmail.com",
+        status: "active",
+    },
+    {
+        id: 3,
+        name: "Top Kek",
+        email: "w@l.com",
+        status: "inactive",
+    },
+];
+
+app.get("/api/members", (req, res) => {
+    res.json(members);
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 
