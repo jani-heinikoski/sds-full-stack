28d5df54acf1563194ab9fd570e74b17c06b450e
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2022-07-27 22:29:39 +0300

    express module: route for getting a single member with id

--- coursework/expressjs/index.js
+++ coursework/expressjs/index.js
@@ -14,6 +14,18 @@ app.get("/api/members", (req, res) => {
     res.json(members);
 });
 
+// Get a single member
+app.get("/api/members/:id", (req, res) => {
+    const member = members.filter(
+        (mber) => mber.id === parseInt(req.params.id)
+    );
+    if (member.length === 0) {
+        res.status(400).json({ msg: `no member with id ${req.params.id}` });
+    } else {
+        res.json(member);
+    }
+});
+
 // Serve static assets from the /public path
 app.use(express.static(path.join(__dirname, "public")));
 
