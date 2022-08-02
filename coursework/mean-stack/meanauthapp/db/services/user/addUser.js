afdf63e55d057d391fd53546a4db9d07145fb26e
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2022-08-02 22:08:52 +0300

    mean module: part 3 finished

---
+++ coursework/mean-stack/meanauthapp/db/services/user/addUser.js
@@ -0,0 +1,9 @@
+const bcryptjs = require("bcryptjs");
+
+module.exports = async (user) => {
+    const salt = await bcryptjs.genSalt(10);
+    const hash = await bcryptjs.hash(user.password, salt);
+    user.password = hash;
+    user.salt = salt;
+    return await user.save();
+};
--- coursework/mean-stack/meanauthapp/routes/users.js
+++ coursework/mean-stack/meanauthapp/routes/users.js
@@ -1,9 +1,31 @@
 const express = require("express");
+// DB Models
+const User = require("../db/models/user");
+// DB Services
+const addUser = require("../db/services/user/addUser");
+const getUserById = require("../db/services/user/getUserById");
+const getUserByUsername = require("../db/services/user/getUserByUsername");
 
 const router = express.Router();
 
-router.get("/register", (req, res) => {
-    res.send("<h1>Register</h1>");
+router.post("/register", async (req, res) => {
+    try {
+        await addUser(
+            new User({
+                name: req.body.name,
+                email: req.body.email,
+                username: req.body.username,
+                password: req.body.password,
+                salt: null,
+            })
+        );
+    } catch (err) {
+        console.error(err);
+        return res
+            .status(400)
+            .json({ success: false, msg: "Failed to register user." });
+    }
+    return res.status(400).json({ success: true, msg: "User registered." });
 });
 
 router.get("/authenticate", (req, res) => {
