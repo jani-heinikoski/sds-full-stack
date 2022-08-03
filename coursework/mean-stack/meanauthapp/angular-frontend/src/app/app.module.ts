9d9ae53461276dbfb3c15dcad99ff48f05a8e4d0
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2022-08-03 13:15:52 +0300

    mean module: generated components + added bootswatch sandstone css

--- coursework/mean-stack/meanauthapp/angular-frontend/src/app/app.module.ts
+++ coursework/mean-stack/meanauthapp/angular-frontend/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    DashboardComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
