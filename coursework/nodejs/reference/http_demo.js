cb05b44df4b31ecb2e213e76926935b2ddf7626b
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2022-08-04 22:04:48 +0300

    checked in only the meanauthapp from mean-stack module

--- coursework/nodejs/reference/http_demo.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-
-// Create a server object
-http.createServer((req, res) => {
-    // send a res
-    res.write("hello world");
-    res.end();
-}).listen(3000, () => console.log("server running on port 3000"));
